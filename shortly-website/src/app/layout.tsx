9efd5655c8fa8f0d62a7f018bdc370b388be9a4d
Nicolas Korsunski <84050410+letmegro@users.noreply.github.com>
2023-09-17 22:54:01 -0400

    created next js app

---
+++ shortly-website/src/app/layout.tsx
@@ -0,0 +1,29 @@
+import './globals.css'
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+import s from "../../public/images/favicon-32x32.png"
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'Shortly',
+  description: 'a frontend website for shortening links (semi functional)',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com"/>
+        <link rel="preconnect" href="https://fonts.gstatic.com"/>
+        <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;700&display=swap" rel="stylesheet"/>
+        <link rel='shortcut icon'  type='image/png' href="images/favicon-32x32.ico"/>
+      </head>
+      <body className={inter.className}>{children}</body>
+    </html>
+  )
+}
---
+++ shortly-website/tailwind.config.ts
@@ -0,0 +1,30 @@
+import type { Config } from 'tailwindcss'
+
+const config: Config = {
+  content: [
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        cyan: 'hsl(180, 66%, 49%)',
+        cyanLight: 'hsl(180, 66%, 69%)',
+        darkViolet: 'hsl(257, 7%, 63%)',
+        red: 'hsl(0, 87%, 67%)',
+        grayishViolet: 'hsl(257, 7%, 63%)',
+        veryDarkBlue: 'hsl(255, 11%, 22%)',
+        veryDarkViolet: 'hsl(260, 8%, 14%)',
+      },
+      fontFamily: {
+        sans: ['Poppins', 'sans-serif'],
+      },
+      spacing: {
+        180: '32rem',
+      },
+    },
+  },
+  plugins: [],
+}
+export default config
