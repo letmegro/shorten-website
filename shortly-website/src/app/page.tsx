4538fd982fe52569315c78b50e36b92895f5435c
Nicolas Korsunski <84050410+letmegro@users.noreply.github.com>
2023-09-19 22:20:17 -0400

    Update page.tsx

--- shortly-website/src/app/page.tsx
+++ shortly-website/src/app/page.tsx
@@ -105,34 +105,6 @@ export default function Home() {
               </button>
             </div>
           </div>
-          {/* link 2 */}
-          <div className='flex flex-col items-center justify-between w-full p-6 bg-white rounded-lg md:flex-row'>
-            <p className='font-bold text-center text-veryDarkViolet md:text-left'>
-              https://twitter.com/frontendmentor
-            </p>
-            <div className='flex-col flex items-center justify-end flex-1 space-x-4 space-y-2 md:flex-row md:space-y-0'>
-              <div className='font-bold text-cyan'>
-                https://rel.ink/k9Gyyk
-              </div>
-              <button className='p-2 px-8 text-white bg-cyan rounded-lg hover:opacity-70 focus:outline-none'>
-                Copy
-              </button>
-            </div>
-          </div>
-          {/* link 3 */}
-          <div className='flex flex-col items-center justify-between w-full p-6 bg-white rounded-lg md:flex-row'>
-            <p className='font-bold text-center text-veryDarkViolet md:text-left'>
-              https://linkedin.com/frontend-mentor
-            </p>
-            <div className='flex-col flex items-center justify-end flex-1 space-x-4 space-y-2 md:flex-row md:space-y-0'>
-              <div className='font-bold text-cyan'>
-                https://rel.ink/k0pKyk
-              </div>
-              <button className='p-2 px-8 text-white bg-cyan rounded-lg hover:opacity-70 focus:outline-none'>
-                Copy
-              </button>
-            </div>
-          </div>
         </div>
       </section>
       {/* stats section */}
